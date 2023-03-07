9046fcdbb2e8734ed8bba855055ab63d877311d5
nima karimian <niimak97@gmail.com>
2023-03-07 22:12:05 +0330

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,29 +21,33 @@ public class Main {
         Session session =databaseConfiguration.Config();
         Transaction transaction = session.beginTransaction();
         //Read-Fulljoin
-        Query query = session.createQuery("select e.firstName,e.lastName,d.departmentName from Employee e " +
-                                                  "full join department d on e.department.id = d.id"
-                                                  );
-
-        List<Object[]> employees =(List<Object[]>) query.getResultList();
-        for (Object[] elem :
-                employees) {
-            System.out.println(elem[0]+" "+elem[1]+" "+elem[2]+" ");
-        }
-
+//        Query query = session.createQuery("select e.firstName,e.lastName,d.departmentName from Employee e " +
+//                                                  "full join department d on e.department.id = d.id"
+//                                                  );
+//
+//        List<Object[]> employees =(List<Object[]>) query.getResultList();
+//        for (Object[] elem :
+//                employees) {
+//            System.out.println(elem[0]+" "+elem[1]+" "+elem[2]+" ");
+//        }
+//
 
 
         //Read-InnerJoin
-//        Query secondQuery = session.createQuery("select department.departmentName, project .projectName from department inner join " +
-//                                                        "proje");
+        Query secondQuery = session.createQuery("select department.departmentName, project.projectName from department inner join " +
+                                                        "project.departments pd on pd.id=department.id");
+        List<Object[]> hqldepartmentsProjectsList = (List<Object[]>) secondQuery.getResultList();
+        for (Object[] elem:hqldepartmentsProjectsList){
+            System.out.println(elem[0]+ " "+ elem[1]);
+        }
             //SQL JOIN ON DEPARTMENT - PROJECT - DEPARTMENT_PROJECT
-            Query thirdQuery  = session.createSQLQuery("select department.departmentname , project.projectname from department" +
-                                                               " join project_department on department.id = project_department.departments_id" +
-                                                               " join project on project_department.projects_id = project.id");
-            List<Object[]> departmentsProjectsList = (List<Object[]>) thirdQuery.getResultList();
-            for (Object[] elem:departmentsProjectsList){
-                System.out.println(elem[0]+ " "+ elem[1]);
-            }
+//            Query thirdQuery  = session.createSQLQuery("select department.departmentname , project.projectname from department" +
+//                                                               " join project_department on department.id = project_department.departments_id" +
+//                                                               " join project on project_department.projects_id = project.id");
+//            List<Object[]> sqldepartmentsProjectsList = (List<Object[]>) thirdQuery.getResultList();
+//            for (Object[] elem:sqldepartmentsProjectsList){
+//                System.out.println(elem[0]+ " "+ elem[1]);
+//            }
 
 //        select public.department.departmentname , public.project.projectname from public.department
 //        join public.project_department on department.id = project_department.departments_id join public.project on project_department.projects_id = project.id
